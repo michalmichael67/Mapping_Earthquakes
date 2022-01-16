52e82051638ba37d100db7e3ee9e8d6a8e5985c6
Michal Michael <michalmichael@Michals-iMac.lan>
2022-01-16 12:03:26 -0500

    Adding multiple points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,32 +2,29 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 
-//add marker for Los Angeles
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//add an array of cities data
+let cityData = cities;
 
-// add a circle on los angeles
-//let circle = L.circle([34.0522, -118.2437], {
-    //radius: 300,
-    //color: "black",
-    //fillColor: "yellow"
-//}).addTo(map);
-
-//use circle marker to add a marker
-let circleMarker = L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: "#ffffa1"
-}).addTo(map);
+// loop through cities and add a marker on each
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: "orange",
+    fillColor: "orange"
+  })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " +city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-  //  maxZoom: 18,
-  //  accessToken: API_KEY
+//    maxZoom: 18,
+//    accessToken: API_KEY
 // });
-
 
 // dark version of the map
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -36,6 +33,7 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z
   id: "dark-v10",
   accessToken: API_KEY
 });
+
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
